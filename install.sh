46d6165d4ed1e8bab9f3a359cf630c1bb30d9d11
mxcl <mxcl@me.com>
2022-05-26 10:53:07 -0400

    Prepping for this script working

--- install.sh
+++ install.sh
@@ -11,9 +11,13 @@ if [ "$1" = "--show" ] && [ $2 = "twitter" ]; then
 elif [ -n "$TEA_SECRET" ]; then
   # Hi, I know you’re excited but genuinely, pls wait for release
   # I added this so I can do CI :/
-  case $(uname) in
-  Darwin)
+  case $(uname)-$(uname -m) in
+  Darwin-arm64)
     MIDFIX=darwin/aarch64;;
+  Darwin-amd64)
+    MIDFIX=darwin/x86-64;;
+  Linux-amd64)
+    MIDFIX=linux/x86-64;;
   *)
     echo "unsupported OS or architecture" >&2
     exit 1;;
@@ -21,14 +25,19 @@ elif [ -n "$TEA_SECRET" ]; then
 
   if [ ! -f /usr/local/bin/tea ]; then
     tmp=$(mktemp)
-    curl https://$TEA_SECRET/tea.xyz/$MIDFIX/tea -o $tmp
-    sudo mkdir -p /usr/local/bin
-    sudo mv $tmp /usr/local/bin/tea
-    sudo chmod u+x /usr/local/bin/tea
+    curl https://$TEA_SECRET/tea.xyz/$MIDFIX/v'*'.tar.gz -o $tmp
     sudo mkdir -p /opt
     sudo chgrp staff /opt
     sudo chmod g+w /opt
+    cd /opt
+    tar xzf $tmp
+    cd tea.xyz
+    ln -sf $(ls -d */ | head -n 1) v'*'  #FIXME
 
+    sudo mkdir -p /usr/local/bin
+    sudo ln -sf /opt/tea.xyz/v'*'/bin/tea /usr/local/bin/tea
+
+    echo >> ~/.zshrc
     echo '# added by tea' >> ~/.zshrc
     echo 'add-zsh-hook -Uz chpwd (){ source <(tea -Eds) }' >> ~/.zshrc
   fi
