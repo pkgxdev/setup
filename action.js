c52db49bc8de939793bbe3dfec2d382c684722db
Max Howell <mxcl@me.com>
2022-09-08 08:31:10 -0400

    Become relocatable
    Default install is ~/.tea for end-users, and ~/opt on GHA. Builds still require /opt to avoid potential issues with spaces and to ensure we are not battling other bugs from build location inconsistencies.

--- action.js
+++ action.js
@@ -1,32 +1,51 @@
 const { execSync } = require('child_process')
 const fs = require('fs')
+const os = require("os")
 
 try {
-  process.stdout.write("Installing tea…\n")
+  process.stdout.write("installing tea…\n")
 
   const GITHUB_TOKEN = process.env['INPUT_TOKEN'].trim()
-  execSync(`${__dirname}/install.sh`, {} , {
-    stdio: "inherit",
-    env: { GITHUB_TOKEN }
-  })
+  const PREFIX = process.env['INPUT_PREFIX'].trim() || `${os.homedir()}/opt`
+
+  let out = execSync(`${__dirname}/install.sh`, {
+    env: {
+      ...process.env,
+      GITHUB_TOKEN,
+      PREFIX,
+      YES: '1',
+      FORCE: '1'
+      //^^ so running this twice doesn’t do unexpected things
+      //^^ NOTE ideally we would have a flag to just abort if already installed
+    }
+  }).toString()
+
+  const v = out.trim().split("\n").slice(-1)[0].match(/\d+\.\d+\.\d+/)[0]
+  const GITHUB_PATH = process.env['GITHUB_PATH']
+  const bindir = `${PREFIX}/tea.xyz/v${v}/bin`
+  fs.appendFileSync(GITHUB_PATH, `${bindir}\n`, {encoding: 'utf8'})
+
+  const teafile = `${bindir}/tea`
 
   const target = process.env['INPUT_TARGET']
   if (target) {
-    execSync(`/usr/local/bin/tea ${target}`, {
+    execSync(`${teafile} ${target}`, {
       stdio: "inherit",
       env: { GITHUB_TOKEN }
     })
   }
 
-  const out = execSync('/usr/local/bin/tea -Eds').toString()
+  out = execSync(`${teafile} -Eds`).toString()
   const match = out.match(/export VERSION=(.*)/)
   if (match && match[1]) {
     const version = match[1]
     process.stdout.write(`::set-output name=version::${version}\n`)
 
-    const path = process.env['GITHUB_ENV']
-    fs.appendFileSync(path, `VERSION=${version}\n`, {encoding: 'utf8'})
+    const GITHUB_ENV = process.env['GITHUB_ENV']
+    fs.appendFileSync(GITHUB_ENV, `VERSION=${version}\n`, {encoding: 'utf8'})
   }
+
+  process.stdout.write(`::set-output name=prefix::${PREFIX}`)
 
 } catch (err) {
   console.error(err)
