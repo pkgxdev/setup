6f93e8985ecac8d545e18f016ecfe6a222bb9603
Jacob Heider <jacob@tea.xyz>
2022-10-31 17:36:10 -0400

    update discussions link

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ prepare() {
 		MIDFIX=linux/x86-64;;
 	*)
 		echo "tea: error: (currently) unsupported OS or architecture ($HW_TARGET)" >&2
-		echo "let’s talk about it: https://github.com/teaxyz/cli/discussions" >&2
+		echo "let’s talk about it: https://github.com/orgs/teaxyz/discussions" >&2
 		exit 1;;
 	esac
 
@@ -45,7 +45,7 @@ prepare() {
 		MAJOR=$(sw_vers -productVersion | cut -d . -f 1)
 		if test "$MAJOR" -lt 11; then
 			echo "tea: error: we currently don't support macOS versions less than 11" >&2
-			echo "let’s talk about it: https://github.com/teaxyz/cli/discussions" >&2
+			echo "let’s talk about it: https://github.com/orgs/teaxyz/discussions" >&2
 			exit 1
 		fi
 	fi
