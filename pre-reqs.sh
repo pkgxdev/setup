d060dedca1144defddbde265e91b2b0c5a555abb
Max Howell <mxcl@me.com>
2022-12-21 13:04:49 -0500

    wip

--- pre-reqs.sh
+++ pre-reqs.sh
@@ -4,17 +4,13 @@ if test -f /etc/debian_version; then
   apt-get --yes update
 
   case $(cat /etc/debian_version) in
-  jessie/sid)
-    apt-get --yes install libc-dev libstdc++-4.8-dev libgcc-10-dev;;
-  stretch/sid)
-    apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
+  jessie/sid|stretch/sid)
+    apt-get --yes install libc-dev libstdc++-4.8-dev libgcc-4.7-dev;;
   buster/sid)
     apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev;;
   bullseye/sid)
     apt-get --yes install libc-dev libstdc++-10-dev libgcc-9-dev;;
-  bookworm/sid)
-    apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
-  *)
+  bookworm/sid|*)
     apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
   esac
 elif test -f /etc/fedora-release; then
