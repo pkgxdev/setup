7e500c001634c0f3b3b957869a04caa3854624db
Max Howell <mxcl@me.com>
2022-12-21 12:03:22 -0500

    wip

--- pre-reqs.sh
+++ pre-reqs.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 if test -f /etc/debian_version; then
-  export DEBIAN_FRONTEND=noninteractive
-  apt-get update
-  apt-get install libc-dev libstdc++-8-dev libgcc-8-dev
+  apt-get --yes update
+  apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev
 fi
