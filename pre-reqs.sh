4b6822f78080e9908920b366c296d29fe85d27dc
Max Howell <mxcl@me.com>
2022-12-21 12:59:39 -0500

    wip

--- pre-reqs.sh
+++ pre-reqs.sh
@@ -4,10 +4,16 @@ if test -f /etc/debian_version; then
   apt-get --yes update
 
   case $(cat /etc/debian_version) in
+  jessie/sid)
+    apt-get --yes install libc-dev libstdc++-4.8-dev libgcc-10-dev;;
+  stretch/sid)
+    apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
+  buster/sid)
+    apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev;;
+  bullseye/sid)
+    apt-get --yes install libc-dev libstdc++-10-dev libgcc-9-dev;;
   bookworm/sid)
     apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
-  *)
-    apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev;;
   esac
 elif test -f /etc/fedora-release; then
   yum --assumeyes install libatomic
