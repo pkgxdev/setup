2665254c6db5ac2f6df4b939a829cabfd6587cf9
Max Howell <mxcl@me.com>
2022-12-21 13:00:08 -0500

    wip

--- pre-reqs.sh
+++ pre-reqs.sh
@@ -14,6 +14,8 @@ if test -f /etc/debian_version; then
     apt-get --yes install libc-dev libstdc++-10-dev libgcc-9-dev;;
   bookworm/sid)
     apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
+  *)
+    apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
   esac
 elif test -f /etc/fedora-release; then
   yum --assumeyes install libatomic
