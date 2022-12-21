e1ac240a3064470d69666520dc11a4b66150a261
Max Howell <mxcl@me.com>
2022-12-21 12:24:56 -0500

    wip

--- pre-reqs.sh
+++ pre-reqs.sh
@@ -2,5 +2,11 @@
 
 if test -f /etc/debian_version; then
   apt-get --yes update
-  apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev
+
+  case $(cat /etc/debian_version) in
+  bookworm/sid)
+    apt-get --yes install libc-dev libstdc++-8-dev libgcc-10-dev;;
+  *)
+    apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev;;
+  esac
 fi
