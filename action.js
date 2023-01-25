5b071fbc8751806d8e923ca58c0fc2d6880a11c7
Max Howell <mxcl@me.com>
2023-01-25 11:08:12 -0500

    wip

--- action.js
+++ action.js
@@ -109,8 +109,10 @@ async function go() {
   for (const line of lines) {
     console.log('hi', line)
     const match = line.match(/(export )?([A-Za-z0-9_]+)=['"](.*)['"]/)
+    console.log(match)
     if (!match) continue
     const [,,key,value] = match
+    console.log(key,value)
     if (key == 'VERSION') {
       fs.appendFileSync(GITHUB_OUTPUT, `version=${value}\n`, {encoding: 'utf8'})
     }
