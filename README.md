cc0fdf24bfd8155dac777f9f6d973bdb26f506ff
Max Howell <mxcl@me.com>
2023-01-31 11:01:28 -0500

    we need netbase on debians
    It has fuck all files in it.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [`install.sh`](./install.sh) is delivered when you `curl tea.xyz`.
 
-# GitHub Action 0.12.4
+# GitHub Action 0.12.5
 
 This repository also provides the `tea` GitHub Action.
 
--- install-pre-reqs.sh
+++ install-pre-reqs.sh
@@ -5,15 +5,19 @@
 if test -f /etc/debian_version; then
   apt-get --yes update
 
+  # minimal but required or networking doesn’t work
+  # https://packages.debian.org/buster/all/netbase/filelist
+  COMMON="netbase"
+
   case $(cat /etc/debian_version) in
   jessie/sid|8.*|stretch/sid|9.*)
-    apt-get --yes install libc-dev libstdc++-4.8-dev libgcc-4.7-dev;;
+    apt-get --yes install libc-dev libstdc++-4.8-dev libgcc-4.7-dev $COMMON;;
   buster/sid|10.*)
-    apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev;;
+    apt-get --yes install libc-dev libstdc++-8-dev libgcc-8-dev $COMMON;;
   bullseye/sid|11.*)
-    apt-get --yes install libc-dev libstdc++-10-dev libgcc-9-dev;;
+    apt-get --yes install libc-dev libstdc++-10-dev libgcc-9-dev $COMMON;;
   bookworm/sid|12.*|*)
-    apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev;;
+    apt-get --yes install libc-dev libstdc++-11-dev libgcc-10-dev $COMMON;;
   esac
 elif test -f /etc/fedora-release; then
   yum --assumeyes install libatomic
