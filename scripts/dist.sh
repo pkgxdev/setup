5daefcbc09c28ed55045378d7c571210e8cff807
Max Howell <mxcl@me.com>
2023-05-28 15:22:59 -0400

    wip

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,17 +1,23 @@
-#!/usr/bin/env tea bash
+#!/usr/bin/env -S tea bash
+
+set -e
 
 source <(tea --magic=bash)
 
 npm i
+
+rm -rf dist
+
 npx -- ncc build action.js --minify --out dist/out
 
-rm -rf dist/build
-mv node_modules/koffi/build dist
+cp -R node_modules/koffi/build dist
 
 cd dist/build/2.*.*/
 rm -rf koffi_*bsd*
 rm -rf *ia32
 rm -rf *koffi_win32*
+rm -rf koffi_linux_arm32hf
+rm -rf koffi_linux_riscv64hf64
 
 cd ../../out
 rm install-pre-reqs.sh
