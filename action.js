c9d315c1b5b027e96ff87681b408ef6bf7ad470f
Max Howell <mxcl@me.com>
2023-05-28 15:36:36 -0400

    use libtea (#155)
    Fixes #158
    
    We now use vercel/ncc as a distribution pre-req because otherwise we’re way too big.
    
    Also we prune the koffi modules down to only what we need for the same reasons.

--- action.js
+++ action.js
@@ -1,17 +1,16 @@
-const { execSync, spawn } = require('child_process')
-const https = require('https')
+const { prefab, hooks, Path, utils, semver, SemVer } = require("@teaxyz/lib")
+const { install, link, resolve, hydrate } = prefab
+const { getExecOutput, exec } = require("@actions/exec")
+const { useConfig, useSync, useCellar } = hooks
+const core = require('@actions/core')
 const path = require('path')
-const fs = require('fs')
 const os = require("os")
 
 async function go() {
-  process.stderr.write("determining latest tea version…\n")
+  const TEA_PREFIX = core.getInput('prefix') || `${os.homedir()}/.tea`
 
-  const PREFIX = process.env['INPUT_PREFIX'] || `${os.homedir()}/.tea`
   const TEA_DIR = (() => {
-    let TEA_DIR = process.env['INPUT_SRCROOT']
-    if (!TEA_DIR) return
-    TEA_DIR = TEA_DIR.trim()
+    let TEA_DIR = core.getInput('srcroot').trim()
     if (!TEA_DIR) return
     if (!TEA_DIR.startsWith("/")) {
       // for security this must be an absolute path
@@ -20,149 +19,115 @@ async function go() {
     return path.normalize(TEA_DIR)
   })()
 
-  const additional_pkgs = []
+  let vtea = core.getInput('version') ?? ""
+  if (vtea && !/^[*^~@=]/.test(vtea)) {
+    vtea = `@${vtea}`
+  }
+
+  const pkgs = [`tea.xyz${vtea}`]
   for (let key in process.env) {
     if (key.startsWith("INPUT_+")) {
       const value = process.env[key]
       if (key == 'INPUT_+') {
         for (const item of value.split(/\s+/)) {
           if (item.trim()) {
-            additional_pkgs.push(`+${item}`)
+            pkgs.push(item)
       }}} else {
-        key = key.slice(6).toLowerCase()
-        additional_pkgs.push(key+value)
+        key = key.slice(7).toLowerCase()
+        pkgs.push(key+value)
   }}}
 
   // we build to /opt and special case this action so people new to
-  // building aren’t immediatelyt flumoxed
-  if (PREFIX == '/opt' && os.platform == 'darwin') {
-    execSync('sudo chown $(whoami):staff /opt')
+  // building aren’t immediately flumoxed
+  if (TEA_PREFIX == '/opt' && os.platform == 'darwin') {
+    await exec('sudo', ['chown', `${os.userInfo().username}:staff`, '/opt'])
   }
 
-  const midfix = (() => {
-    switch (process.arch) {
-    case 'arm64':
-      return `${process.platform}/aarch64`
-    case 'x64':
-      return `${process.platform}/x86-64`
-    default:
-      throw new Error(`unsupported platform: ${process.platform}/${process.arch}`)
-    }
-  })()
+  core.info(`fetching ${pkgs.join(", ")}…`)
 
-  const v = process.env['INPUT_VERSION'] || await new Promise((resolve, reject) => {
-    https.get(`https://dist.tea.xyz/tea.xyz/${midfix}/versions.txt`, rsp => {
-      if (rsp.statusCode != 200) return reject(rsp.statusCode)
-      rsp.setEncoding('utf8')
-      const chunks = []
-      rsp.on("data", x => chunks.push(x))
-      rsp.on("end", () => {
-        resolve(chunks.join("").trim().split("\n").at(-1))
-      })
-    }).on('error', reject)
+  useConfig({
+    prefix: new Path(TEA_PREFIX),
+    pantries: [],
+    cache: new Path(TEA_PREFIX).join('tea.xyz/var/www'),
+    UserAgent: 'tea.setup/0.1.0', //TODO version
+    options: { compression: 'gz' }
   })
-
-  process.stderr.write(`fetching tea.xyz@${v}\n`)
-
-  fs.mkdirSync(PREFIX, { recursive: true })
-
-  const exitcode = await new Promise((resolve, reject) => {
-    https.get(`https://dist.tea.xyz/tea.xyz/${midfix}/v${v}.tar.gz`, rsp => {
-      if (rsp.statusCode != 200) return reject(rsp.statusCode)
-      const tar = spawn('tar', ['xzf', '-'], { stdio: ['pipe', 'inherit', 'inherit'], cwd: PREFIX })
-      rsp.pipe(tar.stdin)
-      tar.on("close", resolve)
-    }).on('error', reject)
-  })
-
-  if (exitcode != 0) {
-    throw new Error(`tar: ${exitcode}`)
+  await useSync()
+  const { pkgs: tree } = await hydrate(pkgs.map(utils.pkg.parse))
+  const { pending } = await resolve(tree)
+  for (const pkg of pending) {
+    core.info(`installing ${utils.pkg.str(pkg)}`)
+    const installation = await install(pkg)
+    await link(installation)
   }
 
-  const oldwd = process.cwd()
-  process.chdir(`${PREFIX}/tea.xyz`)
-  if (fs.existsSync(`v*`)) fs.unlinkSync(`v*`)
-  fs.symlinkSync(`v${v}`, `v*`, 'dir')
-  if (fs.existsSync(`v0`)) fs.unlinkSync(`v0`)
-  fs.symlinkSync(`v${v}`, `v0`, 'dir') //FIXME
-  process.chdir(oldwd)
+  const tea = await useCellar().resolve({project: 'tea.xyz', constraint: new semver.Range('*')})
+  const teafile = tea.path.join('bin/tea').string
+  const env_args = ['--env']
 
-  const GITHUB_PATH = process.env['GITHUB_PATH']
-  const bindir = `${PREFIX}/tea.xyz/v${v}/bin`
-  fs.appendFileSync(GITHUB_PATH, `${bindir}\n`, {encoding: 'utf8'})
-
-  const teafile = `${bindir}/tea`
-
-  const env = {
-    TEA_DIR,
-    ...process.env
+  if (TEA_DIR && tea.pkg.version.gte(new SemVer("0.19"))) {
+    env_args.push('--keep-going')
   }
 
-  const GITHUB_ENV = process.env['GITHUB_ENV']
-  const GITHUB_OUTPUT = process.env['GITHUB_OUTPUT']
+  let args = tea.pkg.version.gte(new SemVer("0.21"))
+    ? []
+    : tea.pkg.version.gte(new SemVer("0.19"))
+      ? ["--dry-run"]
+      : ["--dump"]
 
-  const vv = parseFloat(v)
-  const env_flag = TEA_DIR ? vv >= 0.19 ? '--env --keep-going' : '--env' : ''
-
-  // get env FIXME one call should do init
-
-  let args = vv >= 0.21
-    ? ""
-    : vv >= 0.19
-      ? "--dry-run"
-      : "--dump"
-
-  if (process.env["INPUT_CHASTE"] == "true") {
-    args += " --chaste"
+  if (core.getBooleanInput("chaste")) {
+    args.push('--chaste')
   }
 
-  out = execSync(`${teafile} --sync ${env_flag} ${args} ${additional_pkgs.join(" ")}`, {env}).toString()
+  //FIXME we’re running tea/cli since dev-envs are not in libtea
+  // and we don’t want them in libtea, but we may need a libteacli as a result lol
+  const { stdout: out } = await getExecOutput(
+    teafile,
+    [...env_args, ...args, ...pkgs.map(x=>`+${x}`)],
+    {env: { ...process.env, TEA_DIR, TEA_PREFIX }})
 
   const lines = out.split("\n")
   for (const line of lines) {
     const match = line.match(/(export )?([A-Za-z0-9_]+)=['"]?(.*)/)
     if (!match) continue
     const [,,key,value] = match
-    if (key == 'VERSION') {
-      fs.appendFileSync(GITHUB_OUTPUT, `version=${value}\n`, {encoding: 'utf8'})
-    }
     if (key == 'PATH') {
       for (const part of value.split(":").reverse()) {
-        fs.appendFileSync(GITHUB_PATH, `${part}\n`, {encoding: 'utf8'})
+        core.addPath(part)
       }
     } else {
-      fs.appendFileSync(GITHUB_ENV, `${key}=${value}\n`, {encoding: 'utf8'})
+      core.exportVariable(key, value)
+      if (key == 'VERSION') {
+        core.setOutput('version', value)
+      }
     }
   }
 
   if (TEA_DIR) {
-    fs.appendFileSync(GITHUB_OUTPUT, `srcroot=${TEA_DIR}\n`, {encoding: 'utf8'})
-    fs.appendFileSync(GITHUB_ENV, `TEA_DIR=${TEA_DIR}\n`, {encoding: 'utf8'})
+    core.setOutput('srcroot', TEA_DIR)
+    core.exportVariable('TEA_DIR', TEA_DIR)
   }
 
   if (os.platform() != 'darwin') {
     const sh = path.join(path.dirname(__filename), "install-pre-reqs.sh")
     if (process.getuid() == 0) {
-      execSync(sh)
+      await exec(sh)
     } else {
-      execSync(`sudo ${sh}`)
+      await exec('sudo', [sh])
     }
   }
 
   //TODO deprecated exe/md
   //NOTE BUT LEAVE BECAUSE WE ONCE SUPPORTED THIS
-  const target = process.env['INPUT_TARGET']
+  const target = core.getInput('target')
   if (target) {
-    execSync(`${teafile} ${target}`, {stdio: "inherit", env})
+    await exec(teafile, [target], { env: { ...process.env, TEA_DIR, TEA_PREFIX } })
   }
 
-  fs.appendFileSync(GITHUB_ENV, `TEA_PREFIX=${PREFIX}\n`, {encoding: 'utf8'})
-  fs.appendFileSync(GITHUB_OUTPUT, `prefix=${PREFIX}\n`, {encoding: 'utf8'})
+  core.exportVariable('TEA_PREFIX', TEA_PREFIX)
+  core.setOutput('prefix', TEA_PREFIX)
 
-  process.stderr.write(`installed ${PREFIX}/tea.xyz/v${v}\n`)
+  core.info(`installed ${tea.path}`)
 }
 
-go().catch(err => {
-  console.error(err)
-  process.exitCode = 1
-})
+go().catch(core.setFailed)
---
+++ scripts/dist.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env -S tea bash
+
+set -e
+
+source <(tea --magic=bash)
+
+npm i
+
+rm -rf dist
+
+npx -- ncc build action.js --minify --out dist/out
+
+cp -R node_modules/koffi/build dist
+
+cd dist/build/2.*.*/
+rm -rf koffi_*bsd*
+rm -rf *ia32
+rm -rf *koffi_win32*
+rm -rf koffi_linux_arm32hf
+rm -rf koffi_linux_riscv64hf64
+
+cd ../../out
+rm install-pre-reqs.sh
+ln -s ../../install-pre-reqs.sh
