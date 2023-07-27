aa0024b5412b5aeb24db5433e7c80b5d2a9ddcca
Max Howell <mxcl@me.com>
2023-07-27 10:13:32 -0400

    update installer for v1 (#176)
    The action remains v0 for now. We need the pantry to understand pre-releases or to wait for v1 proper.
    
    only compile dist for releases (#186)
    
    this so dependabot pull requests will work.
    they don’t currently because we require the user to run dist, but that’s unreasonable and bots won’t do it so…
    
    I like this less in that now we have a dist folder committed to main *however* it is not always current :/
    
    Bump actions/checkout from 3 to 4
    
    Bumps [actions/checkout](https://github.com/actions/checkout) from 3 to 4.
    - [Release notes](https://github.com/actions/checkout/releases)
    - [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/actions/checkout/compare/v3...v4)
    
    ---
    updated-dependencies:
    - dependency-name: actions/checkout
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump aws-actions/configure-aws-credentials from 1 to 4
    
    Bumps [aws-actions/configure-aws-credentials](https://github.com/aws-actions/configure-aws-credentials) from 1 to 4.
    - [Release notes](https://github.com/aws-actions/configure-aws-credentials/releases)
    - [Changelog](https://github.com/aws-actions/configure-aws-credentials/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/aws-actions/configure-aws-credentials/compare/v1...v4)
    
    ---
    updated-dependencies:
    - dependency-name: aws-actions/configure-aws-credentials
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump @vercel/ncc from 0.36.1 to 0.38.0
    
    Bumps [@vercel/ncc](https://github.com/vercel/ncc) from 0.36.1 to 0.38.0.
    - [Release notes](https://github.com/vercel/ncc/releases)
    - [Commits](https://github.com/vercel/ncc/compare/0.36.1...0.38.0)
    
    ---
    updated-dependencies:
    - dependency-name: "@vercel/ncc"
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump @teaxyz/lib from 0.6.2 to 0.13.0
    
    Bumps [@teaxyz/lib](https://github.com/teaxyz/lib) from 0.6.2 to 0.13.0.
    - [Release notes](https://github.com/teaxyz/lib/releases)
    - [Commits](https://github.com/teaxyz/lib/compare/v0.6.2...v0.13)
    
    ---
    updated-dependencies:
    - dependency-name: "@teaxyz/lib"
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump @actions/core from 1.10.0 to 1.10.1
    
    Bumps [@actions/core](https://github.com/actions/toolkit/tree/HEAD/packages/core) from 1.10.0 to 1.10.1.
    - [Changelog](https://github.com/actions/toolkit/blob/main/packages/core/RELEASES.md)
    - [Commits](https://github.com/actions/toolkit/commits/HEAD/packages/core)
    
    ---
    updated-dependencies:
    - dependency-name: "@actions/core"
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Bump @types/node from 20.2.5 to 20.6.1
    
    Dependabot couldn't find the original pull request head commit, c2d61a96a5a763c77e8224ddb0eca1ac9d27f0ee.
    
    Bump typescript from 5.1.3 to 5.2.2 (#183)
    
    Bumps [typescript](https://github.com/Microsoft/TypeScript) from 5.1.3 to 5.2.2.
    - [Release notes](https://github.com/Microsoft/TypeScript/releases)
    - [Commits](https://github.com/Microsoft/TypeScript/compare/v5.1.3...v5.2.2)
    
    ---
    updated-dependencies:
    - dependency-name: typescript
      dependency-type: direct:development
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Max Howell <mxcl@me.com>

---
+++ installer.sh
@@ -0,0 +1,128 @@
+#!/bin/sh
+
+set -e
+
+if test -n "$VERBOSE" -o -n "$GITHUB_ACTIONS" -a -n "$RUNNER_DEBUG"; then
+  set -x
+fi
+
+if test -d /usr/local/bin -a ! -w /usr/local/bin; then
+  SUDO="sudo"
+elif test -d /usr/local -a ! -w /usr/local; then
+  SUDO="sudo"
+elif test -d /usr -a ! -w /usr; then
+  SUDO="sudo"
+fi
+
+_install_pre_reqs() {
+  if test -f /etc/debian_version; then
+    apt update --yes
+
+    # minimal but required or networking doesn’t work
+    # https://packages.debian.org/buster/all/netbase/filelist
+    A=netbase
+
+    # difficult to pkg in our opinion
+    B=libudev-dev
+
+    case $(cat /etc/debian_version) in
+    jessie/sid|8.*|stretch/sid|9.*)
+      apt --yes install libc-dev libstdc++-4.8-dev libgcc-4.7-dev $A $B;;
+    buster/sid|10.*)
+      apt --yes install libc-dev libstdc++-8-dev libgcc-8-dev $A $B;;
+    bullseye/sid|11.*)
+      apt --yes install libc-dev libstdc++-10-dev libgcc-9-dev $A $B;;
+    bookworm/sid|12.*|*)
+      apt --yes install libc-dev libstdc++-11-dev libgcc-11-dev $A $B;;
+    esac
+  elif test -f /etc/fedora-release; then
+    $SUDO yum --assumeyes install libatomic
+  fi
+}
+
+_is_ci() {
+  [ -n "$CI" ] && [ $CI != 0 ]
+}
+
+_install_tea() {
+  if _is_ci; then
+    progress="--no-progress-meter"
+  else
+    progress="--progress-bar"
+  fi
+
+  tmpdir=$(mktemp -d)
+
+  if [ $# -eq 0 ]; then
+    echo "Installing: /usr/local/bin/tea" >&2
+
+    # using a named pipe to prevent curl progress output trumping the sudo password prompt
+    pipe="$tmpdir/pipe"
+    mkfifo "$pipe"
+
+    curl $progress --fail --proto '=https' "https://tea.xyz/$(uname)/$(uname -m)".tgz > "$pipe" &
+    $SUDO sh -c "
+      mkdir -p /usr/local/bin
+      tar xz --directory /usr/local/bin < '$pipe'
+    " &
+    wait
+
+    rm -r "$tmpdir"
+
+    export PATH="/usr/local/bin:$PATH"  # just in case
+  else
+    curl $progress --fail --proto '=https' \
+        "https://tea.xyz/$(uname)/$(uname -m)".tgz \
+      | tar xz --directory "$tmpdir"
+
+    export PATH="$tmpdir:$PATH"
+  fi
+
+  unset tmpdir pipe
+}
+
+########################################################################### meat
+
+if ! command -v tea >/dev/null 2>&1; then
+  _install_tea "$@"
+fi
+
+if _is_ci; then
+  apt() {
+    # we should use apt-get not apt in CI
+    # weird shit ref: https://askubuntu.com/a/668859
+    export DEBIAN_FRONTEND=noninteractive
+    cmd=$1
+    shift
+    $SUDO apt-get $cmd -qq -o=Dpkg::Use-Pty=0 $@
+  }
+else
+  apt() {
+    case "$1" in
+    update)
+      echo "ensure you have the `tea` pre-requisites installed:" >&2
+      echo >&2
+      ;;
+    install)
+      echo "   apt-get" "$@" >&2
+      ;;
+    esac
+  }
+  yum() {
+    echo "   yum" "$@" >&2
+  }
+  unset SUDO
+fi
+
+_install_pre_reqs
+
+if [ $# -gt 0 ]; then
+  tea "$@"
+elif [ $(basename "/$0") != 'installer.sh' ]; then
+  # ^^ temporary exception for action.ts
+  eval "$(tea --shellcode)" 2>/dev/null
+
+  if ! _is_ci; then
+    echo "now type: tea --help" >&2
+  fi
+fi
