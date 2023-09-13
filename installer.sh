7aa1b289cf1a82cb9a3a69166723a35c251ba5f2
Max Howell <mxcl@me.com>
2023-09-13 12:57:48 -0400

    wip

--- installer.sh
+++ installer.sh
@@ -94,8 +94,19 @@ _install_pre_reqs
 if [ $# -gt 0 ]; then
   exec tea "$@"
 else
-  echo "$- $0" > file
-  case $- in
-    *e*) eval "$(tea --shellcode)";;
-  esac
+  # https://stackoverflow.com/a/28776166
+  sourced=0
+  if [ -n "$ZSH_VERSION" ]; then
+    case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
+  elif [ -n "$KSH_VERSION" ]; then
+    [ "$(cd -- "$(dirname -- "$0")" && pwd -P)/$(basename -- "$0")" != "$(cd -- "$(dirname -- "${.sh.file}")" && pwd -P)/$(basename -- "${.sh.file}")" ] && sourced=1
+  elif [ -n "$BASH_VERSION" ]; then
+    (return 0 2>/dev/null) && sourced=1
+  else # All other shells: examine $0 for known shell binary filenames.
+    # Detects `sh` and `dash`; add additional shell filenames as needed.
+    case ${0##*/} in sh|-sh|dash|-dash) sourced=1;; esac
+  fi
+  if [ $sourced = 1 ]; then
+    eval "$(tea --shellcode)"
+  fi
 fi
